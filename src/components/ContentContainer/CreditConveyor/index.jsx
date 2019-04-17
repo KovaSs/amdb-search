f00ae7c58f60214091c72ae3dc2c5ea24c6d24d9
Den Isaev <denis.isaev@gazprombank.ru>
2019-04-17 12:43:37 +0300

    add search panel

--- src/components/ContentContainer/CreditConveyor/index.jsx
+++ src/components/ContentContainer/CreditConveyor/index.jsx
@@ -1,58 +1,88 @@
 import React, { Component } from "react";
-import { Row, Col, Form, Input, Button } from "antd";
+import { Row, Col, Form, Input, Button, Select } from "antd";
 import "./сredit-сonveyor.scss"
 
 class CreditConveyor extends Component {
   state = {
-    expand: false,
+    inn: '',
+    ogrn: ''
   };
 
-  handleSearch = e => {
+  handleSubmit = (e) => {
     e.preventDefault();
-    this.props.form.validateFields((err, values) => {
-      console.log("Received values of form: ", values);
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      const {prefix, data} = values;
+      if( prefix === 'inn') {
+        this.setState(({inn}) => ({
+          inn : data,
+          ogrn : ''
+        }))
+      } else if(prefix === 'ogrn') {
+        this.setState(({ogrn, inn}) => ({
+          ogrn : data,
+          inn : '',
+        }))
+      } else {
+        this.setState(({ogrn, inn}) => ({
+          ogrn : '',
+          inn : ''
+        }))
+      }
+      if (!err) {
+        console.log('Полученные значения формы: ', values);
+      }
     });
-  };
+  }
 
-  handleReset = () => {
-    this.props.form.resetFields();
-  };
 
   getFields = () => {
+    const { getFieldDecorator } = this.props.form;
+    const { Option } = Select;
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: 'inn',
+    })(
+      <Select style={{ width: 80 }}>
+        <Option value="inn">ИНН</Option>
+        <Option value="ogrn">ОГРН</Option>
+      </Select>
+    );
     return (
-      <Row gutter={24}>
-        <Col span={24}>
-          <Form.Item label={`ИНН`}>
-            <Input placeholder="Введите ИНН" />
+      <Row>
+        <Col span={24}>Поиск организации:</Col>
+        <Col span={23}>
+          <Form.Item >
+            {getFieldDecorator('data', {
+              rules: [{ required: true, message: 'Строка поиска не должна быть пустой!' }],
+            })(
+              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
+            )}
           </Form.Item>
         </Col>
-        <Col span={24}>
-          <Form.Item label={`ОГРН`}>
-            <Input placeholder="Введите ОГРН" />
-          </Form.Item>
-        </Col>
-        <Col span={6} offset={17}>
-          <Button type="primary" htmlType="submit">Поиск</Button>
+        <Col span={1}>
+          <Button className="search-btn" type="primary" htmlType="submit">Поиск</Button>
         </Col>
       </Row>
     )
   }
 
   render() {
+    // console.log(`state`, this.state)
     return (
       <div className="credit-conveyor">
-        <Col span={5} style={{ backgroundColor: "rgba(43, 255, 0, 0.24)" }}>
-          <Form className="ant-advanced-search-form" onSubmit={this.handleSearch}>
+        <Col style={{ backgroundColor: "rgba(43, 255, 0, 0.24)" }}>
+          <Form className="ant-advanced-search-form" onSubmit={this.handleSubmit}>
             {this.getFields()}
           </Form>
         </Col>
-        <Col
-          span={19}
-          style={{ backgroundColor: "rgba(0, 255, 242, 0.24)" }}
-        />
+        <Col>
+          <div className="search-result-table">Search Result Table</div>
+        </Col>
       </div>
     );
   }
 }
 
-export default CreditConveyor;
+const WrappedRegistrationForm = Form.create({ name: 'searh-company' })(CreditConveyor);
+
+
+export default WrappedRegistrationForm;
