d490b7b3d58247db987becf076e614c048db7abb
Den Isaev <denis.isaev@gazprombank.ru>
2019-04-18 17:42:59 +0300

    show headers info

--- src/components/ContentContainer/CreditConveyor/index.jsx
+++ src/components/ContentContainer/CreditConveyor/index.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Row, Col, Form, Input, Button, Select, Tabs } from "antd";
+import { Row, Col, Form, Input, Button, Select, Tabs, Table, Badge, Menu, Dropdown, Icon  } from "antd";
 import "./сredit-сonveyor.scss"
 
 class CreditConveyor extends Component {
@@ -43,61 +43,160 @@ class CreditConveyor extends Component {
   }
 
   showOrganisationIngo = () => {
+    const columns = [{
+      title: 'Роль',
+      dataIndex: 'name',
+    }, {
+      title: 'За 12 месяцев',
+      dataIndex: 'mounth',
+    }, {
+      title: 'За 3 года',
+      dataIndex: 'year',
+    }];
+    
+    const data = [{
+      key: '1',
+      name: 'Истец',
+      mounth: '0 руб (0)',
+      year: '0 руб (0)',
+    }, {
+      key: '2',
+      name: 'Ответчик',
+      mounth: '500 руб. (1)',
+      year: '500 руб. (1)',
+    }];
+
     return (
-      <div className="tabs-info__organisation">
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Полное наименование</Col>
-          <Col span={18} className="descr">Общество с ограниченной ответственностью "Группа Компаний "Сервис Телеком"</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Сокрашеное наименование</Col>
-          <Col span={18} className="descr">ООО ГК "Сервис Телеком"</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Тип компании</Col>
-          <Col span={18} className="descr">Обычная компания</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Дата регистрации</Col>
-          <Col span={18} className="descr">02.08.2017г.</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Статус</Col>
-          <Col span={18} className="descr">Действующее / Действующее / 15.04.2019</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Юридический адресс</Col>
-          <Col span={18} className="descr">г.Москва, улюПятницкая, д.54 корп.2 кв.5</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Список телефонов</Col>
-          <Col span={18} className="descr">(916)0383738</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Основной ОКВЭД</Col>
-          <Col span={18} className="descr">68.20 / Аренда и управление собственным или арендованным недвижемым имуществом</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Численность персонала</Col>
-          <Col span={18} className="descr">0...5</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Численность персонала по данным ФНС</Col>
-          <Col span={18} className="descr">0</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Уставной капитал</Col>
-          <Col span={18} className="descr">10000</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Индекс должносной осмотрительности</Col>
-          <Col span={18} className="descr">23 / Низкий риск</Col>
-        </Row>
-        <Row className="tabs-info__organisation-info">
-          <Col span={6} className="lable">Индекс Финансового риска</Col>
-          <Col span={18} className="descr">88 / высокий риск</Col>
-        </Row>
-      </div>
+      <Row className="tabs-info__organisation">
+        <Col span={18}>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Полное наименование</Col>
+            <Col span={16} className="descr">Общество с ограниченной ответственностью "Группа Компаний "Сервис Телеком"</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Сокрашеное наименование</Col>
+            <Col span={16} className="descr">ООО ГК "Сервис Телеком"</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Тип компании</Col>
+            <Col span={16} className="descr">Обычная компания</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Дата регистрации</Col>
+            <Col span={16} className="descr">02.08.2017г.</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Статус</Col>
+            <Col span={16} className="descr">Действующее / Действующее / 15.04.2019</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Юридический адресс</Col>
+            <Col span={16} className="descr">г.Москва, улюПятницкая, д.54 корп.2 кв.5</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Список телефонов</Col>
+            <Col span={16} className="descr">(916)0383738</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Основной ОКВЭД</Col>
+            <Col span={16} className="descr">68.20 / Аренда и управление собственным или арендованным недвижемым имуществом</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Численность персонала</Col>
+            <Col span={16} className="descr">0...5</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Численность персонала по данным ФНС</Col>
+            <Col span={16} className="descr">0</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Уставной капитал</Col>
+            <Col span={16} className="descr">10000</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Индекс должносной осмотрительности</Col>
+            <Col span={16} className="descr">23 / Низкий риск</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Индекс Финансового риска</Col>
+            <Col span={16} className="descr">88 / высокий риск</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Исполнительные производства</Col>
+            <Col span={16} className="descr">не найдено</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">ФНС</Col>
+            <Col span={16} className="descr" style={{color : "red"}}>Залоги. Число уведлмлений о заголах движемого имущества (залогодатель) -2</Col>
+          </Row>
+          <Row className="tabs-info__organisation-info">
+            <Col span={8} className="lable">Санкции</Col>
+            <Col span={16} className="descr">не найдено</Col>
+          </Row>
+        </Col>
+        <Col span={6}>
+          <Col className="lable-table">Арбитраж</Col>
+          <Table
+            columns={columns}
+            dataSource={data}
+            bordered
+            pagination={false}
+          />
+        </Col>
+      </Row>
+    )
+  }
+
+  showHeadsTableInfo = () => {
+      const expandedRowRender = () => {
+        const columns = [
+          { title: 'Date', dataIndex: 'date', key: 'date' },
+          { title: 'Name', dataIndex: 'name', key: 'name' },
+          { title: 'Upgrade Status', dataIndex: 'upgradeNum', key: 'upgradeNum' },
+          { title: 'Проверка в Google', dataIndex: 'operation', key: 'operation', render: () => <Button href="javascript:;">Поиск</Button>},
+        ];
+    
+      const data = [];
+      for (let i = 0; i < 3; ++i) {
+        data.push({
+          key: i,
+          date: '2014-12-24 23:12:00',
+          name: 'This is production name',
+          upgradeNum: 'Upgraded: 56',
+        });
+      }
+      return (
+        <Table
+          columns={columns}
+          dataSource={data}
+          pagination={false}
+        />
+      );
+    };
+
+    const columns = [
+      { title: 'ФИО', dataIndex: 'fio', key: 'name' },
+      { title: 'Date', dataIndex: 'createdAt', key: 'createdAt' },
+      { title: 'Проверка', key: 'operation', render: () => <Button href="javascript:;">Поиск</Button> },
+    ];
+
+    const data = [];
+    for (let i = 0; i < 3; ++i) {
+      data.push({
+        key: i,
+        fio: 'Масюгина Жанна Ивановна',
+        createdAt: '2014-12-24 23:12:00',
+      });
+    }
+    
+    return (
+      <Table
+      className="components-table-demo-nested"
+      columns={columns}
+      expandedRowRender={expandedRowRender}
+      dataSource={data}
+      pagination={false}
+    />
     )
   }
 
@@ -105,9 +204,9 @@ class CreditConveyor extends Component {
     const TabPane = Tabs.TabPane;
     return (
       <div className="tabs-info">
-        <Tabs onChange={this.callback} type="card">
+        <Tabs defaultActiveKey="2" onChange={this.callback} >
           <TabPane tab="Организация" key="1">{this.showOrganisationIngo()}</TabPane>
-          <TabPane tab="Руководители" key="2">Content of Tab Pane 2</TabPane>
+          <TabPane tab="Руководители" key="2">{this.showHeadsTableInfo()}</TabPane>
         </Tabs>
       </div>
     )
