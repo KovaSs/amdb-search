b33b9a729cf93cd090d120c2665df872da21cf02
Den Isaev <denis.isaev@gazprombank.ru>
2019-04-20 16:13:27 +0300

    add SearchContainer for redux

---
+++ src/components/ContentContainer/CreditConveyor/SearchCompanyInput/SearchCompanyInput.jsx
@@ -0,0 +1,80 @@
+import React, { Component } from 'react'
+import { Row, Col, Form, Input, Button, Select } from "antd";
+import "./search-company.scss"
+
+
+class SearchCompanyInput extends Component {
+  changeValue = () => {
+    const { actionChangeInn, actionChangeOgrn } = this.props.store
+    setTimeout(() => {
+      switch (this.props.form.setFieldsValue.__reactBoundContext.instances.prefix.props.value) {
+        case 'inn':
+          return actionChangeInn(this.props.form.setFieldsValue.__reactBoundContext.instances.data.state.value) && actionChangeOgrn('')
+        case 'ogrn':
+          return actionChangeOgrn(this.props.form.setFieldsValue.__reactBoundContext.instances.data.state.value) && actionChangeInn('')
+        default:
+          break;
+      }
+      console.log('input-value ->', this.props.form.setFieldsValue.__reactBoundContext.instances.data.state.value)
+      console.log('select-value ->', this.props.form.setFieldsValue.__reactBoundContext.instances.prefix.props.value)
+    }, 200);
+  }
+  
+  handleSubmit = (e) => {
+    console.log('search-value ->', this.props.form)    
+    const { actionChangeInn, actionChangeOgrn } = this.props.store
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Полученные значения формы: ', values);
+        return actionChangeOgrn('') && actionChangeInn('')
+      }
+    });
+  }
+
+  getFields = () => {
+    const { getFieldDecorator } = this.props.form;
+    const { Option } = Select;
+    const prefixSelector = getFieldDecorator('prefix', {
+      initialValue: 'inn',
+    })(
+      <Select style={{ width: 80 }} onChange={this.changeValue}>
+        <Option value="inn">ИНН</Option>
+        <Option value="ogrn">ОГРН</Option>
+      </Select>
+    );
+
+    return (
+      <Row>
+        <Col span={24}>Поиск организации:</Col>
+        <Col span={23}>
+          <Form.Item >
+            {getFieldDecorator('data', {
+              rules: [{ required: true, message: 'Строка поиска не должна быть пустой!' }],
+            })(
+              <Input onChange={this.changeValue} addonBefore={prefixSelector} style={{ width: '100%' }} />
+            )}
+          </Form.Item>
+        </Col>
+        <Col span={1}>
+          <Button className="search-btn" type="primary" htmlType="submit">Поиск</Button>
+        </Col>
+      </Row>
+    )
+  }
+
+  render() {
+    return (
+      <Col>
+        <Form className="ant-advanced-search-form" onSubmit={this.handleSubmit}>
+          {this.getFields()}
+        </Form>
+      </Col>
+    )
+  }
+}
+
+const WrappedRegistrationForm = Form.create({ name: 'searh-company' })(SearchCompanyInput);
+
+
+export default WrappedRegistrationForm
--- src/components/ContentContainer/CreditConveyor/SearchCompanyInput/index.jsx
+++ src/components/ContentContainer/CreditConveyor/SearchCompanyInput/index.jsx
@@ -1,89 +1,10 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from "react-redux";
 import { bindActionCreators } from 'redux';
 import { actionChangeInn, actionChangeOgrn } from "../../../../store/creditConveyor/actions";
-import { Row, Col, Form, Input, Button, Select } from "antd";
-import "./search-company.scss"
+import SearchCompanyInput from "./SearchCompanyInput";
 
-
-class SearchCompanyInput extends Component {
-  state = {
-    inn: '',
-    ogrn: '',
-  };
-
-  handleSubmit = (e) => {
-    const { actionChangeInn, actionChangeOgrn } = this.props
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      const {prefix, data} = values;
-      if( prefix === 'inn' && data) {
-        actionChangeInn(data)
-        this.setState(({inn}) => ({
-          inn : data,
-          ogrn : '',
-        }))
-      } else if(prefix === 'ogrn'  && data) {
-        actionChangeOgrn(data)
-        this.setState(({ogrn, inn}) => ({
-          ogrn : data,
-          inn : '',
-        }))
-      } else {
-        this.setState(({ogrn, inn}) => ({
-          ogrn : '',
-          inn : '',
-        }))
-      }
-      if (!err) {
-        console.log('Полученные значения формы: ', values);
-      }
-    });
-  }
-
-  getFields = () => {
-    const { getFieldDecorator } = this.props.form;
-    const { Option } = Select;
-    const prefixSelector = getFieldDecorator('prefix', {
-      initialValue: 'inn',
-    })(
-      <Select style={{ width: 80 }}>
-        <Option value="inn">ИНН</Option>
-        <Option value="ogrn">ОГРН</Option>
-      </Select>
-    );
-
-    return (
-      <Row>
-        <Col span={24}>Поиск организации:</Col>
-        <Col span={23}>
-          <Form.Item >
-            {getFieldDecorator('data', {
-              rules: [{ required: true, message: 'Строка поиска не должна быть пустой!' }],
-            })(
-              <Input addonBefore={prefixSelector} style={{ width: '100%' }} />
-            )}
-          </Form.Item>
-        </Col>
-        <Col span={1}>
-          <Button className="search-btn" type="primary" htmlType="submit">Поиск</Button>
-        </Col>
-      </Row>
-    )
-  }
-
-  render() {
-    return (
-      <Col>
-        <Form className="ant-advanced-search-form" onSubmit={this.handleSubmit}>
-          {this.getFields()}
-        </Form>
-      </Col>
-    )
-  }
-}
-
-const WrappedRegistrationForm = Form.create({ name: 'searh-company' })(SearchCompanyInput);
+const Container = props => <SearchCompanyInput store={props}/>
 
 const putStateToProps = state => {
   const {creditConveyor : {inn, ogrn}} = state
@@ -99,4 +20,4 @@ const putActionsToProps = dispatch => {
   }
 }
 
-export default connect(putStateToProps, putActionsToProps)(WrappedRegistrationForm)
+export default connect(putStateToProps, putActionsToProps)(Container)
