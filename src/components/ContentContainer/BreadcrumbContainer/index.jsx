9c4b336e2722ca92af2296c860f88709274b9e24
Den Isaev <denis.isaev@gazprombank.ru>
2019-04-25 14:09:56 +0300

    add breadcrumb

--- src/components/ContentContainer/BreadcrumbContainer/index.jsx
+++ src/components/ContentContainer/BreadcrumbContainer/index.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import { Breadcrumb } from "antd";
 
-const BreadcrumbContainer = () => {
+const BreadcrumbContainer = props => {
+  const { pathname } = props.location
+  console.log('pathname', pathname)
+
+  const renderPageName = pathname => {
+    switch (pathname) {
+      case "/open-bill":
+        return "Открыть счет"
+      case "/credit-conveyor":
+        return "Кредитный конвейер"
+      case "/electronic-bank-garantees":
+        return "Электронные банковские гарантии"
+      case "/early-warning-system":
+        return "Система раннего предупреждения"
+      default:
+        break;
+    }
+  }
+
   return (
     <Breadcrumb style={{ margin: "16px 0" }}>
-      <Breadcrumb.Item>User</Breadcrumb.Item>
-      <Breadcrumb.Item>Bill</Breadcrumb.Item>
+      <Breadcrumb.Item>Главная</Breadcrumb.Item>
+      <Breadcrumb.Item>{renderPageName(pathname)}</Breadcrumb.Item>
     </Breadcrumb>
   )
 }
 
-export default BreadcrumbContainer
+export default withRouter(BreadcrumbContainer)
