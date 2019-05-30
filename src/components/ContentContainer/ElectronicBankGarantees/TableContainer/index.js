b9b7847fa1b024f7b16f9721ff45c2aa8fb52cf2
Den Isaev <denis.isaev@gazprombank.ru>
2019-05-30 10:17:55 +0300

    add rendering server data in bank garanties

--- src/components/ContentContainer/ElectronicBankGarantees/TableContainer/index.js
+++ src/components/ContentContainer/ElectronicBankGarantees/TableContainer/index.js
@@ -1,38 +1,44 @@
 import React, { Component } from 'react'
 import { Table, Input, Button, Icon } from 'antd';
 import Highlighter from 'react-highlight-words';
+import moment from 'moment'
 
 const data = [
   {
     key: '1',
-    number: 'John Brown',
-    date: 32,
-    object: 'New York No. 1 Lake Park',
+    number: '1',
+    date: moment('2010-12-08').format('DD.MM.YYYY'),
+    info: {
+      name: 'ШАМКОВ МАКСИМ АНАТОЛЬЕВИЧ',
+      inn: '552801899036',
+      birthday: '1969-04-22',
+    }
   },
   {
     key: '2',
-    number: 'Joe Black',
-    date: 42,
-    object: 'London No. 1 Lake Park',
-  },
-  {
-    key: '3',
-    number: 'Jim Green',
-    date: 32,
-    object: 'Sidney No. 1 Lake Park',
-  },
-  {
-    key: '4',
-    number: 'Jim Red',
-    date: 32,
-    object: 'London No. 2 Lake Park',
+    number: '2',
+    date: moment('2010-12-09').format('DD.MM.YYYY'),
+    info: {
+      name: "ОБЩЕСТВО С ОГРАНИЧЕННОЙ ОТВЕТСТВЕННОСТЬЮ \"БЕЛЫЙ ДОМ\"",
+      inn: '2901178314',
+      ogrn: '1082901005263',
+    }
   },
 ];
 
 class TableContainer extends Component {
   state = {
     searchText: '',
+    loading: false,
+    tableData: ''
   };
+
+  componentDidMount() {
+    this.setState({loading : true})
+    setTimeout(() => {
+      this.setState({loading : false, dataTable : data})
+    }, 3000);
+  }
 
   getColumnSearchProps = dataIndex => ({
     filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
@@ -52,10 +58,10 @@ class TableContainer extends Component {
           size="small"
           style={{ width: 90, marginRight: 8 }}
         >
-          Search
+          Поиск
         </Button>
         <Button onClick={() => this.handleReset(clearFilters)} size="small" style={{ width: 90 }}>
-          Reset
+          Сбросить
         </Button>
       </div>
     ),
@@ -68,7 +74,7 @@ class TableContainer extends Component {
 
     onFilterDropdownVisibleChange: visible => {
       if (visible) {
-      setTimeout(() => this.searchInput.select());
+        setTimeout(() => this.searchInput.select());
       }
     },
 
@@ -93,35 +99,53 @@ class TableContainer extends Component {
   };
 
   render() {
-  const columns = [
-    {
-      title: '№ п/п',
-      dataIndex: 'number',
-      key: 'number',
-      width: '10%',
-      ...this.getColumnSearchProps('number'),
-    },
-    {
-      title: 'Дата поступления',
-      dataIndex: 'date',
-      key: 'date',
-      width: '20%',
-      ...this.getColumnSearchProps('date'),
-    },
-    {
-      title: 'Объект запроса',
-      dataIndex: 'object',
-      key: 'object',
-      ...this.getColumnSearchProps('object'),
-    },
-    {
-      title: 'Взять в работу',
-      dataIndex: 'object',
-      key: 'object',
-      ...this.getColumnSearchProps('object'),
-    },
-  ];
-  return <Table bordered columns={columns} dataSource={data} />;
+    const columns = [
+      {
+        title: '№ п/п',
+        dataIndex: 'number',
+        key: 'number',
+        width: '10%',
+        ...this.getColumnSearchProps('number'),
+      },
+      {
+        title: 'Дата поступления',
+        dataIndex: 'date',
+        key: 'date',
+        width: '20%',
+        ...this.getColumnSearchProps('date'),
+      },
+      {
+        title: 'Объект запроса',
+        key: 'object',
+        render: (text, record) => (
+          <div>
+            <div> {`${ record.info.name }`} </div>
+            <small><b>ИНН:</b> { record.info.inn }</small>{"  "}
+            { record.info.ogrn ?
+              <small><b>ОГРН:</b> { record.info.ogrn }</small> :
+              <small><b>Дата рождения:</b> {  `${moment(record.info.birthday).format('DD.MM.YYYY')}г.` }</small> 
+            }
+          </div>
+        ),
+      },
+      {
+        title: '',
+        key: 'action',
+        render: (text, record) => (
+          <Button> Взять в работу </Button>
+        )
+      },
+    ];
+
+    const { dataTable, loading } = this.state
+    return <Table
+      bordered
+      columns={columns}
+      dataSource={dataTable}
+      loading = {loading}
+      title={() => 'Электронные банковские гарантии'}
+      size="small"
+    />;
   }
 }
 
